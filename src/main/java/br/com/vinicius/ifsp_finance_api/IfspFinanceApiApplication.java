a5db9160e53035a2c1d7b78fd78ef407915f9f00
Felipe Tsuda <felipe.tsuda@pecaai.com.br>
2024-11-26 08:42:05 -0300

    init: start project

---
+++ src/main/java/br/com/vinicius/ifsp_finance_api/IfspFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.ifsp_finance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfspFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfspFinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vinicius/ifsp_finance_api/IfspFinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vinicius.ifsp_finance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfspFinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
